34297b96fc459d3dbf9a2b5fcb31476acc8f3a5d
zengkai <1965599425.qq.com>
2020-09-01 18:00:52 +0800

    rules

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,25 @@
 const path = require('path')
-modules.exports = {
+module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname,'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                'file-loader'
+                ]
+            }
+        ]
     }
 }
