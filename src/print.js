2f13f792c8f47fe8e048333bf02c0234cf493476
zengkai <1965599425@qq.com>
2020-09-15 17:12:55 +0800

    热更新

---
+++ src/print.js
@@ -0,0 +1,4 @@
+export default function printMe() {
+    console.log('Updating print.js...2')
+    // console.log('I get called from print.js!');
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,39 @@
-const path = require('path')
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const webpack = require('webpack');
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname,'dist')
+    entry: {
+        app: './src/index.js',
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist',
+        hot: true
     },
     module: {
         rules: [
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader'
-                ]
-            },
-            {
-                test: /\.(png|svg|jpg|gif)$/,
-                use: [
-                'file-loader'
-                ]
-            }
+           {
+               test: /\.css$/,
+               use: ['style-loader', 'css-loader']
+           }
         ]
+    },
+    plugins: [
+        new CleanWebpackPlugin({
+            cleanOnceBeforeBuildPatterns:'[dist]'
+        }),
+        new HtmlWebpackPlugin({
+            title: 'Output Management'
+        }),
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    optimization: {
+        namedModules: true
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname,'dist'),
+        publicPath: '/'
     }
-}
+}   
