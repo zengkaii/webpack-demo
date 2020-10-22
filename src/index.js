6e030264100bbcee491757f5f943aeb5b2217bf1
zengkai <1965599425@qq.com>
2020-10-22 18:00:31 +0800

    懒加载

--- src/index.js
+++ src/index.js
@@ -8,12 +8,26 @@ async function getComponent() {
 
    // }).catch(error => 'An error occurred while loading the component')
    var element = document.createElement('div')
-   const _ = await import(/* webpackChunkName: "lodash" */ 'lodash')
+   // const _ = await import(/* webpackChunkName: "lodash" */ 'lodash')
 
-   element.innerHTML = _.join(['Hello', 'webpack'], ' ')
+   // element.innerHTML = _.join(['Hello', 'webpack'], ' ')
+   var button = document.createElement('button');
+   var br = document.createElement('br');
 
+   button.innerHTML = 'Click me and look at the console!';
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+   element.appendChild(br);
+   element.appendChild(button);
+
+   // Note that because a network request is involved, some indication
+   // of loading would need to be shown in a production-level site/app.
+   button.onclick = e => import(/* webpackChunkName: "print" */ './print').then(module => {
+     var print = module.default;
+
+     print();
+   });
    return element
 }
-getComponent().then(component => {
-   document.body.appendChild(component)
-})
+document.body.appendChild(component())
+// getComponent().then(component => {
+// })
